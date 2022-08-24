5afcace3128cf828a044634cfdfc69e264539a63
katy4599 <katy.millard@gmail.com>
2022-08-24 15:09:09 -0500

    delete form from contact slide

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -12,28 +12,6 @@
           <img class="socialLink" src="@/assets/Email.png" />
         </a>
       </div>
-      <form name="contact" method="POST" content-type="application/x-www-form-urlencoded" data-netlify="true" netlify>
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Your Role: <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-
-      
     </div>  
       
 </template>
