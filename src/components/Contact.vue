b679d1dd566ce2a69c26e511529b87b7d50a8690
katy4599 <katy.millard@gmail.com>
2022-08-27 18:54:55 -0500

    add form

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -12,7 +12,25 @@
           <img class="socialLink" src="@/assets/Email.png" />
         </a>
       </div>
-      
+      <div>
+        <form @submit.prevent="handleSubmit" name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+          <p style="display: none;" class="hidden">
+            <label>Don't fill this out if you are human: <input name="bot-field" /></label>
+          </p>
+          <p>
+            <label>Your Name: <input v-model="form.name" type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input v-model="form.email" type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <textarea v-model="form.message" name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
     </div>  
       
 </template>
@@ -22,11 +40,36 @@ export default {
   name: 'Contact',
   props: {
   },
-  data() {
-        return {
-            github: 'https://github.com/katy4599',
-            linkedin: 'https://www.linkedin.com/in/katy-millard'
-        }
+  data: () => ({
+    form: {
+      name: '',
+      email: '',
+      message: ''
+    },
+    github: 'https://github.com/katy4599',
+    linkedin: 'https://www.linkedin.com/in/katy-millard'
+  }),
+  
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&')
+    },
+    handleSubmit() {
+      fetch('/', {
+        method: 'post',
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: this.encode({
+          'form-name': 'contact',
+          ...form
+        })
+      })
+      .then(() => console.log('successfully sent'))
+      .catch(e => console.error(e))
+    }
   }
 }
 </script>
@@ -35,7 +78,7 @@ export default {
 
 .contact {
   text-align: center;
-  margin-top: 22.5%;
+  margin-top: 2.5%;
   font-family: Ubuntu, Helvetica, Arial, sans-serif;
   display: flex;
   flex-direction: column;
