0973d39dfbe75609a11407761f1c2cd399a917bd
katy4599 <katy.millard@gmail.com>
2022-08-28 16:34:57 -0500

    add style tag to sub fail

--- src/components/SubmissionFail.vue
+++ src/components/SubmissionFail.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Oops, looks like something went wrong!</h1>
     <router-link to="/">
-      <button>Back to form</button>
+      <button>Back to Home</button>
     </router-link>
   </div>
 </template>
@@ -12,3 +12,20 @@
     name: "SubmissionFail"
   }
 </script>
+
+<style>
+
+html {
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  height: 100%;
+  background-color: #b1efe5;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  font-family: Ubuntu, Helvetica, Arial, sans-serif;
+}
+
+</style>
